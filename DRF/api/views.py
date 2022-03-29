870cc00b639404e594e08b74a3eaf745fdb2ac2c
Elijah <reinhardelijah1@gmail.com>
2022-03-29 15:14:50 +0300

    feat new finctionality and new file permissons to proj

--- DRF/api/views.py
+++ DRF/api/views.py
@@ -1,14 +1,35 @@
 from DRF.api.models import Snippet
 from DRF.api.serializers import SnippetSerializer
 from rest_framework import generics
+from DRF.api.serializers import UserSerializer
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from DRF.api.permissions import IsOwnerOrReadOnly
 
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
+
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
