9464e8d0d8e51c8ac5a828cb50c954205baba601
Elijah <reinhardelijah1@gmail.com>
2022-03-31 16:47:46 +0300

    feat: open-api and open-cli for data in json

--- DRF/api/urls.py
+++ DRF/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from DRF.api.views import SnippetViewSet, UserViewSet, api_root
-from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
 from DRF.api import views
+from rest_framework.schemas import get_schema_view
 
 
+schema_view = get_schema_view(title='Pastebin API')
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
 ]
